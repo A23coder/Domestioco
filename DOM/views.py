788ac7ea76a55be6ba0b9c65cf05427cfdfaaece
AP23 <aakash2panchal@gmail.com>
2022-11-04 17:16:59 +0530

    gi

--- DOM/views.py
+++ DOM/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Home Page is showing like hot summer")
+
+def home():
+    return HttpResponse("Hi this is home Page <h3>Hello My name is Aakash</h3>")
