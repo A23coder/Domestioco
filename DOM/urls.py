c10a9d27b605ce9697b86b2370434e221fc9fe36
AP23 <aakash2panchal@gmail.com>
2022-11-05 11:00:06 +0530

    changes check

--- DOM/urls.py
+++ DOM/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("",views.index,name="index"),
     path("home",views.home,name=""),
+    path("about",views.home,name=""),
+    path("contact",views.home,name=""),
+    path("services",views.home,name=""),
 ]
--- DOM/views.py
+++ DOM/views.py
@@ -4,5 +4,14 @@ from django.shortcuts import render,HttpResponse
 def index(request):
     return HttpResponse("Home Page is showing like hot summer")
 
-def home():
+def home(request):
     return HttpResponse("Hi this is home Page <h3>Hello My name is Aakash</h3>")
+
+def about(request):
+    return HttpResponse("Hi this is About page <h3>Hello My name is Aakash</h3>")
+
+def contact(request):
+    return HttpResponse("Hi this is contact page <h3>Hello My name is Aakash</h3>")
+    
+def services(request):
+    return HttpResponse("Hi this is service Page <h3>Hello My name is Aakash</h3>")
