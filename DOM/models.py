0963cf8137cda34390fdac7adf075ae96f745149
AP23 <aakash2panchal@gmail.com>
2022-11-07 10:19:55 +0530

    minor changes
    commit by aakash dt=7-11-22

--- DOM/models.py
+++ DOM/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Contacts(models.Model):
     name=models.CharField(max_length=20)
     email=models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+    
